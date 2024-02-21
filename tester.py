271e82279196813c685fb5cde6cb8ed7ea124430
Aaruni Kaushik <aaruni96@users.noreply.github.com>
2024-02-21 13:45:48 +0100

    Ak96/multitesting (#2)
    Generate jobs dynamically

--- tester.py
+++ tester.py
@@ -1,29 +1,20 @@
-import os
+import sys
 import requests
-import yaml
 import subprocess
 
-url = '''https://raw.githubusercontent.com/oscar-system/oscar-website/gh-pages/_data/examples.yml'''
+print(sys.argv)
 
-yamlfile = requests.get(url).content
+repo = sys.argv[1]
+filename = sys.argv[2]
+print(f"Grabbing {repo}/{filename}...")
+tut = f'''https://raw.githubusercontent.com/{repo}/master/{filename}.ipynb'''
+nbfilename = f"notebooks/{filename}.ipynb"
+with open(nbfilename, 'w') as tutfile:
+    r = requests.get(tut).content.decode()
+    tutfile.write(r)
 
-yamlcontent = yaml.safe_load(yamlfile)
-
-try:
-    os.mkdir("notebooks")
-except Exception as e:
-    print(e)
-
-for tutorial in yamlcontent:
-    print(f"Grabbing {tutorial}...")
-    tut = f'''https://raw.githubusercontent.com/{tutorial["repository"]}/master/{tutorial["filename"]}.ipynb'''
-    nbfilename = f"notebooks/{tutorial['filename']}.ipynb"
-    with open(nbfilename, 'w') as tutfile:
-        r = requests.get(tut).content.decode()
-        tutfile.write(r)
-    
-    #finally, run the stuff
-    print(f"Running {tutorial}...")
-    subprocess.run(f'''jupytext --set-kernel "julia-1.10" --execute {nbfilename}''', shell=True, check=True)
-    print(f"{tutorial} tested successfully!")
+#finally, run the stuff
+print(f"Running {repo}/{filename}...")
+subprocess.run(f'''jupytext --set-kernel "julia-1.10" --execute {nbfilename}''', shell=True, check=True)
+print(f"{repo}/{filename} tested successfully!")
 
